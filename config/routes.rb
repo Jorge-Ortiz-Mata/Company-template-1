2bf6a65385578b0d903b255f97e4838e4d798e08
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-03-25 00:45:49 -0600

    Add Omniauth for Google.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks', sessions: 'users/sessions' }
   root 'pages#index'
   get 'pages/about'
   get 'pages/contact'
+
+  devise_scope :user do
+    get 'sign_out' => 'devise/sessions#destroy'
+  end
+
+  get 'omniauth_test', to: 'home#display_omniauth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
