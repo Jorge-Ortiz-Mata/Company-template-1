8a52f769212aa275ff72611cf8846961a32fc599
Jorge-Ortiz <ortiz.mata.jorge@gmail.com>
2022-03-26 10:43:22 -0600

    Restart commit.

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -27,37 +27,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       redirect_to new_user_registration_url
     end
   end
-=begin
-  def github
-    @user = User.from_omniauth(request.env["omniauth.auth"])
-    after_create_callbacks if @user.new_record? && @user.save
 
-    if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-      set_flash_message(:notice, :success, kind: "Github") if is_navigational_format?
-    else
-      session["devise.github_data"] = request.env["omniauth.auth"].except(:extra) # Removing extra as it can overflow some session stores
-      redirect_to new_user_registration_url
-    end
-  end
-=end
   def failure
     redirect_to root_path
   end
-
-  def upgrade
-    scope = nil
   
-    if params[:provider] == "facebook"
-      scope = 'email,offline_access,user_about_me,publish_stream'
-    end
-  
-    redirect_to user_omniauth_authorize_path(params[:provider]), flash: {scope: scope}
-  end
-
-  def setup
-    request.env['omniauth.strategy'].options['scope'] = flash[:scope] || request.env['omniauth.strategy'].options['scope']
-    render :text => "Setup complete.", :status => 404
-  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,9 +8,6 @@ Rails.application.routes.draw do
     get 'sign_out' => 'devise/sessions#destroy'
   end
 
-  get '/users/auth/:provider/upgrade' => 'omniauth_callbacks#upgrade', as: :user_omniauth_upgrade
-  get '/users/auth/:provider/setup', :to => 'omniauth_callbacks#setup'
-
   get 'omniauth_test', to: 'home#display_omniauth'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
